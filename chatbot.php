0e2268835828d47dee7a81e7bc79ce0738873cd5
Bruce Wayne <macbookpro@Bruce-Wayne-2.local>
2017-01-28 23:58:36 +0200

    update fb url

--- chatbot.php
+++ chatbot.php
@@ -1,9 +1,9 @@
 <?php
-
+// git add . git commit -am ""; git push heroku master
 // configurations
 $secret_token = 'test_token';
 $fb_access_token = 'EAAJ2HjwuKgoBANfPntPbhnvUXkbzOWHbqoZAEnvfFcGJccgEK7CZCAnvET7i5B5HA1VLCLuNwejEZC4mtchKZABSuPewCcscTltF7ItqzI13RTo7BKU0h1XuNMQgBbONBDKyxPCWHGAp8ZCOCyVTQQhObBy7hDMmHGCn5CIQNeAZDZD';
-$url = "https://graphs.facebook.com/v2.6/me/messages?access_token=$fb_access_token";
+$url = "https://graph.facebook.com/v2.6/me/messages?access_token=$fb_access_token";
 $received_secret_token = '';
 
 // check if request is for challenge
