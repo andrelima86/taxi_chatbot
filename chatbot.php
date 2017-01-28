174c73a05156f685a6c0bc78c70f955bc4993bf6
Bruce Wayne <macbookpro@Bruce-Wayne-2.local>
2017-01-28 12:15:51 +0200

    make it better

---
+++ chatbot.php
@@ -0,0 +1,13 @@
+<?php
+
+$secret_token = 'test_token';
+
+if(isset($_REQUEST['hub_challenge'])){
+	$challenge = $_REQUEST['hub_challenge'];
+	$received_secret_token = $_REQUEST['hub_verify_token'];	
+}
+
+if(strcmp($secret_token, $received_secret_token) === 0){
+	echo $challenge;
+	exit();
+}
